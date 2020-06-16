2606089e315d3fa5e57c4e86fc0b263c7d241cf6
Argenis Ochoa Gonzalez <argenis8agonzalez@gmail.com>
2020-06-16 17:43:09 -0500

    deleted:    Sshd/Dockerfile 	deleted:    Sshd/README.md 	deleted:    Sshd/docker-compose.yml 	deleted:    Sshd/docker-entrypoint.sh 	deleted:    Sshd/etc/authorized_keys

--- Sshd/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ -z "${AUTHORIZED_KEYS}"]; then
-   echo "Please is necessary public key as AUTHORIZED_KEYS env variable."
-   exit 1
-fi
-
-echo "Maybe /root/.ssh/authorized_keys "
-echo "${AUTHORIZED_KEYS}" > /root/.ssh/authorized_keys
