ee66aaefd79f55a38ba92d6a2667461bdf542dbb
Argenis Ochoa Gonzalez <argenis8agonzalez@gmail.com>
2020-10-23 14:00:47 -0500

    Agregando shadowsocks generador de codigo qr

--- shadowsocks/dockerfile/docker-entrypoint.sh
+++ shadowsocks/dockerfile/docker-entrypoint.sh
@@ -7,7 +7,7 @@ fi
 if [[ -f "/var/run/secrets/$PASSWORD_SECRET" ]]; then
     PASSWORD=$(cat "/var/run/secrets/$PASSWORD_SECRET")
 fi
-
+exec qrencode -t ansiutf8 ss://$PASSWORD@$IPEXTERNA:$SERVER_PORT &
 exec ssserver \
       -s $SERVER_ADDR \
       -p $SERVER_PORT \
